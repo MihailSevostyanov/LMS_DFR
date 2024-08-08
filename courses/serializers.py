061a172ea99918fb529033cd1745919011fcf660
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-08-08 17:25:08 +0300

    ДЗ 24.2 Задание 1

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from courses.models import Course, Lesson
 
@@ -7,6 +7,19 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_lesson = SerializerMethodField(
+        read_only=True, help_text="Число уроков в курсе"
+    )
+
+    def get_count_lesson(self, obj):
+        return Lesson.objects.filter(course=obj).count()
+
+    class Meta:
+        model = Course
+        fields = ("title", "preview", "description", "count_lesson")
 
 
 class LessonSerializer(ModelSerializer):
