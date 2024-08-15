6827e41cba969461b50d5ad165cf5ee5025a01b4
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-08-15 15:54:58 +0300

    ДЗ 25.1 Задание 1

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserViewSet, PaymentsListAPIView, PaymentsCreateAPIView, PaymentsRetrieveAPIView, PaymentsUpdateAPIView, PaymentsDestroyAPIView
+from users.views import UserViewSet, PaymentsListAPIView, PaymentsCreateAPIView, PaymentsRetrieveAPIView, \
+    PaymentsUpdateAPIView, PaymentsDestroyAPIView, UserCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -10,6 +13,9 @@ router = SimpleRouter()
 router.register("", UserViewSet)
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny)), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny)), name='token_refresh'),
     path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
     path("payments/create/", PaymentsCreateAPIView.as_view(), name="payments_create"),
     path("payments/<int:pk>/", PaymentsRetrieveAPIView.as_view(), name="payments_retrieve"),
